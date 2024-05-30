70e0e464afc0568bde40d340144a0fcadae8eef6
Nishan Bista <nishanbista525@gmail.com>
2024-05-30 14:12:20 +0545

    feat: responsive category card

--- src/Components/CateCard.tsx
+++ src/Components/CateCard.tsx
@@ -2,21 +2,26 @@ import type { CategoryCard } from "../Types";
 
 function CateCard({ category }: { category: CategoryCard }) {
   return (
-    <div className="bg-Second rounded h-[180px] w-[150px] text-bodyBg">
-      <div id="image&title" className="h-[80%]">
-        <div
-          id="image"
-          className="w-full h-[95px]  bg-cover "
-          style={{ backgroundImage: `url(${category.strCategoryThumb})` }}
-        ></div>
-        <div className="flex items-center justify-center text-xl border-t-2 border-Third mt-2 pt-1 ">
-          {category.strCategory}
+    <div className="bg-Second rounded h-[180px] w-[150px] text-bodyBg flex tablet:w-[500px] pb-2">
+      <div className="h-full w-[150px]">
+        <div id="image&title" className="h-[80%]">
+          <div
+            id="image"
+            className="w-full h-[95px]  bg-cover my-2 "
+            style={{ backgroundImage: `url(${category.strCategoryThumb})` }}
+          ></div>
+          <div className="flex items-center justify-center text-xl border-t-2 border-Third mt-2 pt-1 ">
+            {category.strCategory}
+          </div>
+        </div>
+        <div id="button" className="flex justify-center mt-0.5 ">
+          <button className="w-[90%] border rounded-lg transition duration-300 ease-in-out transform scale-100  hover:scale-90 hover:bg-bodyBg hover:border-1 hover:text-black active:scale-75  active:border-green-400 ">
+            Browse
+          </button>
         </div>
       </div>
-      <div id="button" className="flex justify-center mt-0.5">
-        <button className="w-[90%] border rounded-lg transition duration-300 ease-in-out transform scale-100  hover:scale-90 hover:bg-bodyBg hover:border-1 hover:text-black active:scale-75  active:border-green-400 ">
-          Browse
-        </button>
+      <div className="text-sm w-[350px] hidden tablet:block border-l border-Third p-3 overflow-y-scroll scrollbar-minimal">
+        {category.strCategoryDescription}
       </div>
     </div>
   );
--- src/Pages/Pages/Categories.tsx
+++ src/Pages/Pages/Categories.tsx
@@ -16,7 +16,9 @@ function Categories() {
 
   return (
     <>
+    <div className="text-4xl  text-center mt-5"> Categories </div>
       <div className="w-full  flex flex-wrap mt-5  gap-3 justify-center">
+        
         {categories.map((item) => (
           <CateCard key={item.idCategory} category={item} />
         ))}
