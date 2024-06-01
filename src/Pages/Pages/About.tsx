69be1fb09a5c22561b2c3677e4e82dd9064bd861
Nishan Bista <nishanbista525@gmail.com>
2024-06-01 20:21:41 +0545

    feat: about pg 1/2

--- src/Pages/Pages/About.tsx
+++ src/Pages/Pages/About.tsx
@@ -1,9 +1,40 @@
-
-
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="pt-10 mx-4">
+      <div
+        id="paragraph"
+        className="text-sm flex flex-col gap-4 text-Text bg-Second p-5 rounded-xl tablet:text-2xl"
+      >
+        <p>
+          Welcome to Recipe Rocket, your go-to destination for discovering and
+          cooking delicious recipes with ease! Our platform leverages the latest
+          technologies, including React, Tailwind CSS, and TypeScript, to
+          provide you with a seamless and visually appealing experience. Whether
+          you're a culinary novice or a seasoned chef, Recipe Rocket offers a
+          vast collection of recipes tailored to suit every taste and dietary
+          preference.
+        </p>
+        <p>
+          At Recipe Rocket, we believe that cooking should be a joyful and
+          accessible experience for everyone. Our intuitive search feature
+          allows you to find the perfect recipe based on your ingredients,
+          favorite cuisines, or dietary needs. Each recipe comes with clear,
+          step-by-step instructions and helpful tips, ensuring that you can
+          confidently create mouthwatering dishes every time. Additionally, we
+          provide nutritional information and user reviews to help you make
+          informed choices and elevate your culinary skills.
+        </p>
+        <p>
+          Join us on this exciting gastronomic journey and let Recipe Rocket
+          ignite your passion for cooking. With our extensive recipe collection
+          and user-friendly interface, you'll find inspiration and guidance to
+          create delicious meals effortlessly. Dive into the world of flavors
+          and let your culinary creativity soar with Recipe Rocket, where every
+          recipe is just a search away!
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
