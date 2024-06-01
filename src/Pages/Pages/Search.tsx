193af48e740cce061613842cbb7b6bec6190a020
Nishan Bista <nishanbista525@gmail.com>
2024-06-01 21:26:53 +0545

    feat: search> no results msg

--- src/Pages/Pages/Search.tsx
+++ src/Pages/Pages/Search.tsx
@@ -6,7 +6,6 @@ import Block from "../../Components/Block";
 function Search() {
   const { item } = useParams();
   const [search, setSearch] = useState<Meal[] | null>(null);
-  console.log(search);
 
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`)
@@ -19,10 +18,20 @@ function Search() {
     <>
       <div>
         <div className="text-2xl mt-4">Showing Results for: {item}</div>
-        <div id="items" className="flex items-center flex-col gap-y-5 mt-6">
-          {search?.map((data) => (
-            <Block key={data.idMeal} meal={data} next={false} />
-          ))}
+        <div
+          id="items"
+          className="flex items-center flex-col gap-y-6 mt-6 tablet:flex-row tablet:flex-wrap tablet:justify-center tablet:gap-x-8"
+        >
+          {search ? (
+            search.map((data) => (
+              <Block key={data.idMeal} meal={data} next={false} />
+            ))
+          ) : (
+            <div className="text-2xl mt-28  tablet:text-3xl">
+              No Results Found for{" "}
+              <span className="text-Second font-bold italic">{item}</span>!
+            </div>
+          )}
         </div>
       </div>
     </>
