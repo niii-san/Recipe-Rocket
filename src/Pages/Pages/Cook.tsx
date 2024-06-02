067594c491fd0498c33e79f6d721230f79a09db0
Nishan Bista <nishanbista525@gmail.com>
2024-06-02 21:23:22 +0545

    feat: ingredients-measures table

--- src/Pages/Pages/Cook.tsx
+++ src/Pages/Pages/Cook.tsx
@@ -15,9 +15,31 @@ function Cook() {
       .then((res) => res.json())
       .then((data) => setCurrentMeal(data.meals[0]));
   }, [meal]);
+
+  const renderIngredients = () => {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = currentMeal?.[`strIngredient${i}` as keyof Meal];
+      if (ingredient && ingredient.trim()) {
+        ingredients.push(<div key={i}>{ingredient}</div>);
+      }
+    }
+    return ingredients;
+  };
+
+  const renderMeasures = () => {
+    const measures = [];
+    for (let i = 1; i <= 20; i++) {
+      const measure = currentMeal?.[`strMeasure${i}` as keyof Meal];
+      if (measure && measure.trim()) {
+        measures.push(<div key={i}>{measure}</div>);
+      }
+    }
+    return measures;
+  };
   return (
     <>
-      <div className="flex flex-col items-center pt-5 w-full ">
+      <div className="flex flex-col items-center pt-5 w-full pb-72 ">
         <div
           id="title-image-tags"
           className="flex flex-col items-center bg-Second text-Text rounded-xl "
@@ -45,9 +67,22 @@ function Cook() {
           </div>
           <div
             id="instructionData"
-            className="bg-Second text-Text text-sm border border-Third p-4 rounded-xl max-h-[400px] overflow-y-scroll"
+            className="bg-Second text-Text text-sm border border-Third p-4 rounded-xl max-h-[400px] overflow-y-scroll scrollbar-minimal"
           >
             {currentMeal?.strInstructions}
+          </div>
+        </div>
+        <div
+          id="ingredients-measures"
+          className="bg-Second flex w-[96%] text-Text mt-6 rounded-xl"
+        >
+          <div id="ingredients" className=" w-[50%] p-3">
+            <h1 className="text-2xl border-b border-Third mb-3">Ingredients</h1>
+            <div>{renderIngredients()}</div>
+          </div>
+          <div id="measures" className=" w-[50%] border-l border-Third p-3">
+            <h1 className="text-2xl border-b border-Third mb-3">Measures</h1>
+            <div className="">{renderMeasures()}</div>
           </div>
         </div>
       </div>
