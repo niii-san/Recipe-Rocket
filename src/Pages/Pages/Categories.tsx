6baf39891a6a2364efca6749731641d402120e69
Nishan Bista <nishanbista525@gmail.com>
2024-06-03 20:40:00 +0545

    feat: categories skeleton

--- src/Pages/Pages/Categories.tsx
+++ src/Pages/Pages/Categories.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import CateCard from "../../Components/CateCard";
 import type { CategoryCard } from "../../Types";
+import Skeleton from "../../Components/Skeleton";
 
 function Categories() {
   const [categories, setCategories] = useState<CategoryCard[] | []>([]);
-
 
   useEffect(() => {
     fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
@@ -18,9 +18,16 @@ function Categories() {
     <>
       <div className="text-4xl  text-center mt-5"> Categories </div>
       <div className="w-full  flex flex-wrap mt-5  gap-3 justify-center pc:gap-">
-        {categories.map((item) => (
-          <CateCard key={item.idCategory} category={item} />
-        ))}
+        {categories.length > 0
+          ? categories.map((item) => (
+              <CateCard key={item.idCategory} category={item} />
+            ))
+          : [...Array(14)].map((_, i) => (
+              <Skeleton
+                key={i}
+                className="h-[180px] w-[150px] tablet:w-[500px]"
+              />
+            ))}
       </div>
     </>
   );
